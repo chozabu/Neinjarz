376a6de644bfd51ebea671b4ab1711f465d3d483
chozabu <chozabu@gmail.com>
2016-11-18 16:52:13 +0000

    seperate main menu out into its own section

--- menu.py
+++ menu.py
@@ -23,9 +23,40 @@ def addLogText(*items):
     if len(log) > 10:
         log = log[1:]
 
+class MainMenu():
+    def handleEvent(self, event):
+        if event.type == 2:#keypress
+            if event.key == pgl.K_r:
+                terrain.loadimg("clev")
+            if event.key == pgl.K_w:
+                simplenet.isServer = 0
+                simplenet.isClient = 0
+                return True
+            if event.key == pgl.K_s:
+                simplenet.isServer = 1
+                simplenet.isClient = 0
+                simplenet.initNet()
+                return True
+            if event.key == pgl.K_c:
+                simplenet.isServer = 0
+                simplenet.isClient = 1
+                self.name = "nameless"+str(random()*1000)[0:3]
+                simplenet.sendserv("join", self.name)
+                return True
+    def draw(self):
+        displayratio = display.sh*0.01
+        tx = -20*displayratio
+        display.drawWord(pos=[-display.sw2+20,-display.sh2+200,0*displayratio], string="NeinJarz", color=[1,1,1],size=displayratio*40)
+        display.drawWord(pos=[tx,0*displayratio,0], string="press W to do single player", color=[1,1,1],size=displayratio*20)
+        display.drawWord(pos=[tx,5*displayratio,0], string="press S to Host a game", color=[1,1,1],size=displayratio*20)
+        display.drawWord(pos=[tx,10*displayratio,0], string="press C to Join a game", color=[1,1,1],size=displayratio*20)
+        display.drawWord(pos=[tx,15*displayratio,0], string="press R to Reset Level", color=[1,1,1],size=displayratio*20)
+
 class Menu():
     def __init__(self):
         self.clearLog()
+        self.mainMenu = MainMenu()
+        self.currentMenu = self.mainMenu
     def clearLog(self):
         global log
         log = ["This is the log", "press m for the menu"]
@@ -36,38 +67,19 @@ class Menu():
             #time.sleep(1.0/60.0)
             timer.tick()
             for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
                 if event.type == 2:#keypress
                     if event.key == pgl.K_ESCAPE:
                         sys.exit()
-                    if event.key == pgl.K_r:
-                        terrain.loadimg("clev")
-                    if event.key == pgl.K_w:
-                        simplenet.isServer = 0
-                        simplenet.isClient = 0
-                        inmenu = 0
-                    if event.key == pgl.K_s:
-                        simplenet.isServer = 1
-                        simplenet.isClient = 0
-                        inmenu = 0
-                        simplenet.initNet()
-                    if event.key == pgl.K_c:
-                        simplenet.isServer = 0
-                        simplenet.isClient = 1
-                        inmenu = 0
-                        self.name = "nameless"+str(random()*1000)[0:3]
-                        simplenet.sendserv("join", self.name)
                 elif event.type == pgl.VIDEORESIZE:
                     print(event.dict['size'])
                     display.resize(event.dict['size'][0], event.dict['size'][1])
                     pygame.display.flip()
+                if self.currentMenu.handleEvent(event):
+                    inmenu = 0
     def drawMenu(self):
         display.clear()
-        displayratio = display.sh*0.01
-        tx = -20*displayratio
-        display.drawWord(pos=[tx,0*displayratio,0], string="press W to do single player", color=[1,1,1],size=displayratio*20)
-        display.drawWord(pos=[tx,5*displayratio,0], string="press S to Host a game", color=[1,1,1],size=displayratio*20)
-        display.drawWord(pos=[tx,10*displayratio,0], string="press C to Join a game", color=[1,1,1],size=displayratio*20)
-        display.drawWord(pos=[tx,15*displayratio,0], string="press R to Reset Level", color=[1,1,1],size=displayratio*20)
+        self.currentMenu.draw()
         #display.applycam()
         #terrain.draw(display.camx,display.camy)
         #for b in self.walkers:
